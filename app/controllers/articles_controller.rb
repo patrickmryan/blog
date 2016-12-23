e2ad9e7ea89a3f10a4281f1b8088240f86ec5b07
Patrick M. Ryan <pmryan@us.ibm.com>
2016-12-23 13:10:14 -0500

    working on http://edgeguides.rubyonrails.org/getting_started.html, at sec 5.8

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,34 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+ 
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+
+    #self.render(:plain => self.params()[:article].inspect)
+
+    @article = Article.new(article_params())
+
+    @article.save
+    self.redirect_to(@article)
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
