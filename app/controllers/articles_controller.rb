63617eea99a24e2707bf9e5dd2969b24abc7e32e
Patrick M. Ryan <pmryan@us.ibm.com>
2016-12-23 17:08:35 -0500

    before refactoring w/ partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new()
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def create
 
@@ -29,6 +33,16 @@ class ArticlesController < ApplicationController
 
   end
 
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
   def article_params
