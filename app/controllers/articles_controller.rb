831f54d042b0d98120b2024a6786ced2add1c49a
Patrick M. Ryan <pmryan@us.ibm.com>
2016-12-29 10:53:40 -0500

    added basic security.  pmr/secret

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
+
   
+  http_basic_authenticate_with name: "pmr", password: "secret", except: [:index, :show]
+  
+    
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "pmr", password: "secret", only: :destroy
+  
   def create
 
     @article = Article.find(params[:article_id])
